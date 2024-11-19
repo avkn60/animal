06fb0ad6403993199cdc49c0b5cea874035eef34
avkn60 <154770427+avkn60@users.noreply.github.com>
2024-11-19 10:15:48 +0500

    1

---
+++ src/main/java/com/kutik/zoo/repository/AnimalRepository.java
@@ -0,0 +1,10 @@
+package com.kutik.zoo.repository;
+
+import com.kutik.zoo.entity.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal,Long>{
+
+}
